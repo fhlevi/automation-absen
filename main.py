c2d09db70af254d54431c1e2ce79e9ab97c60c46
Levi <faisal@sakoo.id>
2021-12-31 10:51:38 +0700

    feat(condition): added flow for bot absensi

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import config.config_browser as config_browser
 import config.config_env as config_env
 import config.config_date as config_date
 import auto_absen
+import time
 
 config_browser.browser.get(config_env.link)
 
-if((config_date.today != 'sabtu' or config_date.today != 'minggu') and config_env.is_active == 'true'):
-    # login
-    auto_absen.login_access()
+config_browser.browser.set_window_position(-10000,0)
 
-    if config_date.dateNow.hour == 8:
-        if(config_date.dateNow.minute == 00):
+def run():
+    if((config_date.today != 'sabtu' or config_date.today != 'minggu') and config_env.is_active == 'true'):
+        if config_date.dateNow.hour == 8 and config_date.dateNow.minute == 00:
+            # login
+            auto_absen.login_access()
+            
             # checkin
             auto_absen.checkin()
-    else:
-        if(config_date.dateNow.minute == 00):
-            # checkout
-            auto_absen.checkout()
+        else:
+            if(config_date.dateNow.hour == 5 and config_date.dateNow.minute == 00):
+                # login
+                auto_absen.login_access()
+                
+                # checkout
+                auto_absen.checkout()
 
-config_browser.browser.close()
+while True:
+    time.sleep(5)
+    
+    run()
+
+
+
+
 
